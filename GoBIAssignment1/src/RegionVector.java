bd2a6f85ce884ba9cff95861a93afbcd252ddf70
Philloraptor <philippjahn97@web.de>
2017-10-28 14:42:06 +0200

    1.1

--- GoBIAssignment1/src/RegionVector.java
+++ GoBIAssignment1/src/RegionVector.java
@@ -1,29 +1,56 @@
+import java.util.Collections;
+import java.util.Comparator;
 import java.util.Vector;
 
 public class RegionVector {
 
 
-	private Vector<Integer> region;
+	private Vector<Region> regions;
 
 	public RegionVector(int x1, int x2){
-		region.add(x1);
-		region.add(x2);
-		region.sort((a,b) -> Integer.compare(a,b));
+		regions.add(new Region(x1,x2));
+		regions.sort((a,b) -> Integer.compare(a.getStart(),b.getStart()));
 	}
 	
-	public RegionVector(Vector<Integer> region){
-		this.region = region;
-		region.sort((a,b) -> Integer.compare(a,b));
+	public RegionVector(Vector<Region> region){
+		this.regions = region;
+		regions.sort((a,b) -> Integer.compare(a.getStart(),b.getStart()));
 	}
 	
-	public Vector<Integer> getRegion(){
-		return null;
+	public Vector<Region> getRegions(){
+		return regions;
 	}
 	
 	public RegionVector merge(RegionVector rv){
-		Vector<Integer> newregion = (Vector<Integer>) region.clone();
-		newregion.addAll(rv.getRegion());
-		RegionVector mix = new RegionVector(newregion);
-		return mix;
+
+		regions.sort(new RegionComparator());
+
+        Region r = regions.get(0);
+        int start = r.getStart();
+        int stop = r.getStop();
+
+        Vector<Region> resultV = new Vector<Region>();
+
+        for (int i = 1; i < regions.size(); i++) {
+            Region current = regions.get(i);
+            if (current.getStart() <= stop) {
+                stop = Math.max(current.getStop(), stop);
+            } else {
+                resultV.add(new Region(start, stop));
+                start = current.getStart();
+                stop = current.getStop();
+            }
+        }
+
+        resultV.add(new Region(start, stop));
+        return new RegionVector(resultV);
+	}
+	
+	class RegionComparator implements Comparator<Region>
+	{
+	    public int compare(Region x1, Region x2)
+	    {
+	        return x1.getStart() - x2.getStart();
+	    }
 	}
 }
